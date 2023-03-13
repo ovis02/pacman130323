5bc277b6fbcf13b030bb36436d39f3a87ca115c8
ovis02 <oves7860@gmail.com>
2023-03-13 23:27:27 +0100

    deplacement de pacman

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const gameDiv = document.getElementById("game");
-
-//premiere etape crée un plateau("Points à manger,points de puissance, mur,case vide,pac man,fantômes")
-//créer notre pacman
-//gerer ses deplacements (sans contrainte)
-//gerer les contraintes de déplacement (pas dans les murs)
-//pièces à manger
-//gérer les fantômes
+const sizeCaseWidth = 28;
+/*
+OK Créer le plateau (dynamique)
+OK Créer notre pacman
+OK Gérer ses déplacements (sans contrainte)
+OK Contraintes de déplacement (pas dans les murs)
+* Pièces à manger
+* Gérer les fantômes
+* ...
+*/
 
 const layout = [
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -48,10 +51,16 @@ const layout = [
 
 creerPlateau();
 
+//faire bouger le pacman
+document.addEventListener("keyup", (event) => {
+  DeplacerPacman(event.key);
+});
+
 function creerPlateau() {
+  let cptCase = 0;
   layout.forEach((caseLayout) => {
     let casePlateau = document.createElement("div");
-
+    casePlateau.dataset.numerocase = cptCase;
     switch (caseLayout) {
       case 0:
         casePlateau.classList.add("point");
@@ -59,10 +68,8 @@ function creerPlateau() {
       case 1:
         casePlateau.classList.add("mur");
         break;
-      //j'ai changer les cases 1 par mur et je les ai colorié en blanc sur le css
-
       case 2:
-        casePlateau.classList.add("fantôme-area");
+        casePlateau.classList.add("fantome-area");
         break;
       case 3:
         casePlateau.classList.add("point-puissance");
@@ -71,5 +78,54 @@ function creerPlateau() {
         break;
     }
     gameDiv.appendChild(casePlateau);
+    cptCase++;
   });
+
+  getCaseByIndex(489).classList.add("pacman");
+}
+
+function getCaseByIndex(index) {
+  let caseGame = document.querySelector("[data-numerocase='" + index + "']");
+  return caseGame;
+}
+
+function DeplacerPacman(direction) {
+  let pacmanDiv = document.querySelector(".pacman");
+  let pacManCase = pacmanDiv.dataset.numerocase;
+  let caseDestination = null;
+  switch (direction) {
+    case "ArrowUp":
+      //Déplacer la case contenant pacman de 1 vers le haut
+      caseDestination = getCaseByIndex(parseInt(pacManCase) - sizeCaseWidth);
+
+      break;
+    case "ArrowRight":
+      //Déplacer la case contenant pacman de 1 vers la droite
+      caseDestination = getCaseByIndex(parseInt(pacManCase) + 1);
+      break;
+    case "ArrowLeft":
+      //Déplacer la case contenant pacman de 1 vers la gauche
+      caseDestination = getCaseByIndex(parseInt(pacManCase) - 1);
+      break;
+    case "ArrowDown":
+      caseDestination = getCaseByIndex(parseInt(pacManCase) + sizeCaseWidth);
+    default:
+      break;
+  }
+  if (caseDestination != null) {
+    if (checkDirection(caseDestination)) {
+      pacmanDiv.classList.remove("pacman");
+      caseDestination.classList.add("pacman");
+    }
+  }
+}
+
+//return faux si je peux pas aller la où je veux
+//return vrai si je peux
+function checkDirection(caseDestination) {
+  if (caseDestination.classList.contains("mur")) {
+    return false;
+  } else {
+    return true;
+  }
 }
